e5d519942dcddb34070e5b1674d72e07e7c2c97d
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-03-30 17:32:06 +0900

    Add variable assignment and variable reference

--- interp.rb
+++ interp.rb
@@ -1,31 +1,45 @@
 require "minruby"
 
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when "lit"
     tree[1]
   when "*"
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when "/"
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when "+"
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when "-"
-    evaluate(tree[1]) - evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when "%"
-    evaluate(tree[1]) % evaluate(tree[2])
+    evaluate(tree[1], env) % evaluate(tree[2], env)
   when "**"
-    evaluate(tree[1]) ** evaluate(tree[2])
+    evaluate(tree[1], env) ** evaluate(tree[2], env)
   when "=="
-    evaluate(tree[1]) == evaluate(tree[2])
+    evaluate(tree[1], env) == evaluate(tree[2], env)
   when ">"
-    evaluate(tree[1]) > evaluate(tree[2])
+    evaluate(tree[1], env) > evaluate(tree[2], env)
   when ">="
-    evaluate(tree[1]) >= evaluate(tree[2])
+    evaluate(tree[1], env) >= evaluate(tree[2], env)
   when "<"
-    evaluate(tree[1]) < evaluate(tree[2])
+    evaluate(tree[1], env) < evaluate(tree[2], env)
   when "<="
-    evaluate(tree[1]) <= evaluate(tree[2])
+    evaluate(tree[1], env) <= evaluate(tree[2], env)
+  when "func_call"          # 仮の実装
+    p(evaluate(tree[2], env))
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i]
+      last = evaluate(tree[i], env)
+      i += 1
+    end
+    last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
   end
 end
 
@@ -40,7 +54,7 @@ def max(tree)
   end
 end
 
-str = gets
+env = {}
+str = minruby_load()
 tree = minruby_parse(str)
-p "ans:" + evaluate(tree).to_s
-p "max:" + max(tree).to_s
+evaluate(tree, env)

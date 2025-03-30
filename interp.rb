c3ec6cd41d96fa7e3634ff79d8d14bb0a2dd81de
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-03-30 16:49:32 +0900

    Add max method

--- interp.rb
+++ interp.rb
@@ -51,7 +51,18 @@ def evaluate(tree)
   end
 end
 
+def max(tree)
+  p tree
+  if tree[0] == "lit"
+    tree[1]
+  else
+    left = max(tree[1])
+    right = max(tree[2])
+    left >= right ? left : right
+  end
+end
+
 str = gets
 tree = minruby_parse(str)
-ans = evaluate(tree)
-p ans
+p "ans:" + evaluate(tree).to_s
+p "max:" + max(tree).to_s
